c4eba0869c6debda1ce32c3131e76156340d6671
sunner <sunner>
2021-05-17 14:10:28 +0800

    Initial Commit

---
+++ s_callbacktrace/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  s_callbacktrace
+//
+//  Created by sunner on 2021/5/17.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
